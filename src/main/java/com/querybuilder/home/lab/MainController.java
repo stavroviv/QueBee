7d30211671954f386e0626640e5be6b94dfbe43f
IStavrov <petromarz@yandex.ru>
2019-08-07 22:58:19 +0300

    building form

--- src/main/java/com/querybuilder/home/lab/MainController.java
+++ src/main/java/com/querybuilder/home/lab/MainController.java
@@ -20,22 +20,30 @@ public class MainController {
     // loads some strings into the tree in the application UI.
     public void loadTreeItems(String... rootItems) {
 //        final Node rootIcon =  new ImageView(new Image(getClass().getResourceAsStream("/myToolWindow/Calendar-icon.png")));
-        ImageView child =  new ImageView(new Image(getClass().getResourceAsStream("/myToolWindow/plus.png")));
-
-        TreeItem<String> root = new TreeItem<>("Root Node", child);
-        root.setExpanded(true);
-        for (String itemString: rootItems) {
-            root.getChildren().add(new TreeItem<>(itemString, child));
-            root.getChildren().add(new TreeItem<>(itemString, child));
-        }
-
-        TreeItem<String> root2 = new TreeItem<>("Root Node3", child);
-//       root2.setExpanded(true);
-        for (String itemString: rootItems) {
-            root2.getChildren().add(new TreeItem<>(itemString, child));
-            root2.getChildren().add(new TreeItem<>(itemString, child));
-        }
-        root.getChildren().add(root2);
-        locationTreeView.setRoot(root);
+//        ImageView child =  new ImageView(new Image(getClass().getResourceAsStream("/myToolWindow/plus.png")));
+//
+//        TreeItem<String> root = new TreeItem<>("Root Node", child);
+//        root.setExpanded(true);
+//        for (String itemString: rootItems) {
+//            root.getChildren().add(new TreeItem<>(itemString, child));
+//            root.getChildren().add(new TreeItem<>(itemString, child));
+//            root.getChildren().add(new TreeItem<>(itemString, child));
+//            root.getChildren().add(new TreeItem<>(itemString, child));
+//            root.getChildren().add(new TreeItem<>(itemString, child));
+//            root.getChildren().add(new TreeItem<>(itemString, child));
+//            root.getChildren().add(new TreeItem<>(itemString, child));
+//            root.getChildren().add(new TreeItem<>(itemString, child));
+//            root.getChildren().add(new TreeItem<>(itemString, child));
+//            root.getChildren().add(new TreeItem<>(itemString, child));
+//        }
+//
+//        TreeItem<String> root2 = new TreeItem<>("Root Node3", child);
+////       root2.setExpanded(true);
+//        for (String itemString: rootItems) {
+//            root2.getChildren().add(new TreeItem<>(itemString, child));
+//            root2.getChildren().add(new TreeItem<>(itemString, child));
+//        }
+//        root.getChildren().add(root2);
+//        locationTreeView.setRoot(root);
     }
 }
