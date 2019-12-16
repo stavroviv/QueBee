8b05e60199fa854984fd7c21f4575a06cfad5558
IStavrov <petromarz@yandex.ru>
2019-12-16 22:54:50 +0300

    окно новго поля

--- src/main/java/com/querybuilder/home/lab/NewFieldController.java
+++ src/main/java/com/querybuilder/home/lab/NewFieldController.java
@@ -1,9 +1,19 @@
 package com.querybuilder.home.lab;
 
 import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
 public class NewFieldController {
+
+    public Button closeButton;
+
     public void onOkClick(ActionEvent actionEvent) {
-        System.out.println("OK");
+        Window window = ((Node) (actionEvent.getSource())).getScene().getWindow();
+        if (window instanceof Stage) {
+            ((Stage) window).close();
+        }
     }
 }
