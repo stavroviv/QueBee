86760efe2a6d616ccd71de77a5b91165fe8e98d4
IStavrov <petromarz@yandex.ru>
2020-06-15 17:45:10 +0300

    icon

--- src/main/java/com/querybuilder/querypart/CTEPart.java
+++ src/main/java/com/querybuilder/querypart/CTEPart.java
@@ -1,9 +1,9 @@
 package com.querybuilder.querypart;
 
 import com.querybuilder.controllers.MainController;
-import com.querybuilder.domain.*;
+import com.querybuilder.domain.CteRow;
+import com.querybuilder.domain.TableRow;
 import com.querybuilder.domain.qparts.OneCte;
-import com.querybuilder.domain.qparts.Union;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TableColumn;
@@ -66,7 +66,7 @@ public class CTEPart {
     }
 
     public static void setNewName(MainController controller, CteRow cteRow, String oldValue, String newValue) {
-
+        // TODO просто поставить алиса таблицы и не больше
         cteRow.setName(newValue);
         Map<String, OneCte> cteMap = controller.getFullQuery().getCteMap();
         cteMap.get(cteRow.getId()).setCteName(newValue);
@@ -77,55 +77,55 @@ public class CTEPart {
             }
         }
 
-        for (Map.Entry<String, OneCte> oneCteEntry : cteMap.entrySet()) {
-            OneCte oneCte = oneCteEntry.getValue();
-            for (Map.Entry<String, Union> unionEntry : oneCte.getUnionMap().entrySet()) {
-                Union union = unionEntry.getValue();
-                for (TreeItem<TableRow> child : union.getTablesView().getRoot().getChildren()) {
-                    if (child.getValue().getName().equals(oldValue)) {
-                        child.getValue().setName(newValue);
-                    }
-                }
-                for (TableRow item : union.getFieldTable().getItems()) {
-                    if (item.getName().contains(oldValue + ".")) {
-                        item.setName(item.getName().replace(oldValue, newValue));
-                    }
-                }
-                for (LinkElement item : union.getLinkTable().getItems()) {
-                    if (item.getTable1().equals(oldValue)) {
-                        item.setTable1(newValue);
-                    }
-                    if (item.getTable2().equals(oldValue)) {
-                        item.setTable2(newValue);
-                    }
-                    if (item.getCondition().contains(oldValue)) {
-                        item.setCondition(item.getCondition().replace(oldValue, newValue));
-                    }
-                }
-                for (ConditionElement item : union.getConditionTableResults().getItems()) {
-                    if (item.getCondition().contains(oldValue + ".")) {
-                        item.setCondition(item.getCondition().replace(oldValue, newValue));
-                    }
-                }
-                for (TableRow item : union.getGroupTableResults().getItems()) {
-                    if (item.getName().contains(oldValue + ".")) {
-                        item.setName(item.getName().replace(oldValue, newValue));
-                    }
-                }
-            }
-            for (AliasRow item : oneCte.getAliasTable().getItems()) {
-                for (Map.Entry<String, String> aliasEntry : item.getValues().entrySet()) {
-                    if (aliasEntry.getValue().contains(oldValue + ".")) {
-                        aliasEntry.setValue(aliasEntry.getValue().replace(oldValue, newValue));
-                    }
-                }
-            }
-            for (TableRow item : oneCte.getOrderTableResults().getItems()) {
-                if (item.getName().contains(oldValue + ".")) {
-                    item.setName(item.getName().replace(oldValue, newValue));
-                }
-            }
-        }
+//        for (Map.Entry<String, OneCte> oneCteEntry : cteMap.entrySet()) {
+//            OneCte oneCte = oneCteEntry.getValue();
+//            for (Map.Entry<String, Union> unionEntry : oneCte.getUnionMap().entrySet()) {
+//                Union union = unionEntry.getValue();
+//                for (TreeItem<TableRow> child : union.getTablesView().getRoot().getChildren()) {
+//                    if (child.getValue().getName().equals(oldValue)) {
+//                        child.getValue().setName(newValue);
+//                    }
+//                }
+//                for (TableRow item : union.getFieldTable().getItems()) {
+//                    if (item.getName().contains(oldValue + ".")) {
+//                        item.setName(item.getName().replace(oldValue, newValue));
+//                    }
+//                }
+//                for (LinkElement item : union.getLinkTable().getItems()) {
+//                    if (item.getTable1().equals(oldValue)) {
+//                        item.setTable1(newValue);
+//                    }
+//                    if (item.getTable2().equals(oldValue)) {
+//                        item.setTable2(newValue);
+//                    }
+//                    if (item.getCondition().contains(oldValue)) {
+//                        item.setCondition(item.getCondition().replace(oldValue, newValue));
+//                    }
+//                }
+//                for (ConditionElement item : union.getConditionTableResults().getItems()) {
+//                    if (item.getCondition().contains(oldValue + ".")) {
+//                        item.setCondition(item.getCondition().replace(oldValue, newValue));
+//                    }
+//                }
+//                for (TableRow item : union.getGroupTableResults().getItems()) {
+//                    if (item.getName().contains(oldValue + ".")) {
+//                        item.setName(item.getName().replace(oldValue, newValue));
+//                    }
+//                }
+//            }
+//            for (AliasRow item : oneCte.getAliasTable().getItems()) {
+//                for (Map.Entry<String, String> aliasEntry : item.getValues().entrySet()) {
+//                    if (aliasEntry.getValue().contains(oldValue + ".")) {
+//                        aliasEntry.setValue(aliasEntry.getValue().replace(oldValue, newValue));
+//                    }
+//                }
+//            }
+//            for (TableRow item : oneCte.getOrderTableResults().getItems()) {
+//                if (item.getName().contains(oldValue + ".")) {
+//                    item.setName(item.getName().replace(oldValue, newValue));
+//                }
+//            }
+//        }
     }
 
     public static void load(MainController controller) {
